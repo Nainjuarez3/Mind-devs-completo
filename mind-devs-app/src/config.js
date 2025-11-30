8d97c2477f84c7f4415563d7b526bd64af8ae454
Nainjuarez3 <135084497+Nainjuarez3@users.noreply.github.com>
2025-11-30 11:06:45 -0600

    conexion front-back

--- mind-devs-app/src/config.js
+++ mind-devs-app/src/config.js
@@ -2,5 +2,5 @@
 
 // Esta lógica detecta si estás en tu compu o en internet
 export const API_URL = import.meta.env.PROD
-    ? 'https://AQUI-IRA-TU-URL-DE-RENDER.onrender.com'
+    ? 'https://mind-devs-api.onrender.com'
     : 'http://localhost:3000';
